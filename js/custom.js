916402d83aee39ee3c7575e8fd319cc31064e1ab
SweabaSeaban <sweabajilu@gmail.com>
2020-06-01 13:58:59 +0530

    website

---
+++ js/custom.js
@@ -0,0 +1,117 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* Smoothscroll js
+  -----------------------------------------------*/
+    $(function() {
+        $('.navbar-default a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+ /* Home Slideshow Vegas
+  -----------------------------------------------*/
+  $(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'images/slide-img1.jpg' },
+            { src: 'images/slide-img2.jpg' },
+            { src: 'images/slide-img3.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomIn', ],
+        animation: ['kenburns']
+    });
+  });
+
+
+  /* Team carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+      $("#team-carousel").owlCarousel({
+          items : 3,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3],
+          slideSpeed: 300,
+          itemsDesktop : [1199,2],
+          itemsTablet: [768,1],
+          itemsTabletSmall: [985,2],
+          itemsMobile : [479,1],
+      });
+    });
+    
+
+    /* Back to Top
+    -----------------------------------------------*/
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.go-top').fadeIn(200);
+            } else {
+                $('.go-top').fadeOut(200);
+           }
+        });   
+          // Animate the scroll to top
+        $('.go-top').click(function(event) {
+          event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
+/*typed
+---------------------------------*/
+var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Wear your failure as a badge of honour!"],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+
+  // Wrap every letter in a span
+var textWrapper = document.querySelector(' .ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: ' .ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: ' .ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
